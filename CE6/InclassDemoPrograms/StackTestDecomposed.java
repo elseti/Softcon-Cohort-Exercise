868db680e36b432ed35ca192a5d607bfc91e3fe2
Eliana Setiabudi <56292047+elseti@users.noreply.github.com>
2022-06-24 23:11:53 +0800

    did q3; not sure if right though

---
+++ CE6/InclassDemoPrograms/StackTestDecomposed.java
@@ -0,0 +1,66 @@
+//package Week4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class StackTestDecomposed {
+	private Stack<Integer> stack;
+	// setUp method using @Before syntax
+	// @Before methods are run before each test
+	@Before 
+	public void runBeforeEachTest()
+	{
+		System.out.println("setting up");
+		
+	    stack = new Stack<Integer>();
+	}
+
+	// tear-down method using @After
+	// @After methods are run after each test
+	@After 
+	public void runAfterEachTest()
+	{
+	    stack = null;
+		System.out.println("setting down");
+	}
+
+	@Test public void testToString()
+	{
+		System.out.println("testing");
+	   stack.push(new Integer(1));
+	   stack.push(new Integer(2));
+	   assertEquals ("{2, 1}", stack.toString());
+	}
+	
+	//decomposed
+
+	@Test public void testRepOk() {
+		boolean result = stack.repOK();
+		assertEquals (true, result);
+	}
+	
+	
+	@Test public void testPushPop1() {
+		stack.push (new Integer (1));
+		boolean result = stack.repOK();
+		assertEquals (true, result);
+		
+		stack.pop();
+		result = stack.repOK();
+		assertEquals (true, result);
+	}
+	
+	@Test public void testPushPop2() {
+		stack.push (new Integer (1));
+		stack.pop();
+		boolean result = stack.repOK();
+		assertEquals (true, result);
+	}
+	
+	   
+	   
+
+}
